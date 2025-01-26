0e63ef414d8a87491ec053818c4cafc876e4e84e
exporti03 <adzuluaga@outlook.com>
2025-01-26 12:46:06 -0500

    actualizar

---
+++ entradauser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//cout <<(operar de mostrar pantalla)
+//cin >> (operador de insertar texto)
+int main() {
+
+std::string nombre;
+std:: cout <<"Cual es su nombre?";
+std::cin >> nombre;
+
+std::cout << "Hola " <<nombre;
+
+    return 0;
+}
