3ff57d03610dde57eabe2a2f64d1048cc9896f42
exporti03 <adzuluaga@outlook.com>
2025-01-26 14:03:49 -0500

    actualizar

--- entradauser.cpp
+++ entradauser.cpp
@@ -5,10 +5,17 @@
 int main() {
 
 std::string nombre;
-std:: cout <<"Cual es su nombre?";
+int edad;
+
+std:: cout <<"Cual es su nombre? ";
 std::cin >> nombre;
 
-std::cout << "Hola " <<nombre;
+
+std:: cout <<"Cual es su edad? ";
+std::cin >> edad;
+
+std::cout << "Hola " << nombre << '\n';
+std::cout << "Tu edad es "<<edad;
 
     return 0;
 }
