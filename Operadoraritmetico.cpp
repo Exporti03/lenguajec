1592b65590d879c258da9e6cc06992288a5b30a3
exporti03 <adzuluaga@outlook.com>
2025-01-26 11:55:33 -0500

    Aritmetica

--- Operadoraritmetico.cpp
+++ Operadoraritmetico.cpp
@@ -4,10 +4,29 @@
 
 int main() {
 
-    
     int estudiantes = 20;
-    //estudiante = estudiante + 2
+    //SUMA
+    //estudiante = estudiante + 2;
+    //estudiante += 2;
+    //estudiantes++; 
 
+    //RESTA
+    //estudiantes = estudiantes - 2;
+    //estudiantes -= 1;
+
+    //Multiplicar
+    //estudiantes = estudiantes * 2;
+    //estudiantes *= 2;
+
+    //Division
+    //estudiantes = estudiantes / 2;
+    //estudiantes /= 2;
+
+    //modulo (%)
+    int resto = estudiantes % 2;
+
+
+    std::cout<<resto<<std::endl;
 
     return 0;
 }
