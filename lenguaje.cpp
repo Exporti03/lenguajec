864b8946c8a6e02587c4146151d5580edc75ccd8
exporti03 <adzuluaga@outlook.com>
2025-01-29 13:52:28 -0500

    actualizacion

---
+++ lenguaje.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int valor;
+
+    std::cout << "Escribe un valor= ";
+    std::cin >> valor;
+
+    std::cout << "El valor escrito es = " << valor << std::endl;
+
+    return 0;
+}
